67cf16bdfcc9e90269d55bbae97a716bdcf3d64b
w80cI0-liA <spacenomad24@gmail.com>
2024-07-02 19:53:12 +0300

    webpack config development

--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,49 @@
 const path = require('path');
+const miniCss = require('mini-css-extract-plugin');
+
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
-    // use {} for code splitting technique
+    mode: 'development', // production
     entry: {
         slider_js: './src/slider.js',
-        
-        // index: 'index.html',
-        // index_eng: 'index_eng.html',
-        //// Если используется include, то нужно ли обрабатывать включаемый файл?
-        //// metrics: 'metrics.html',
-
-        // media: 'media.css',
-        // slider_css: 'slider.css',
-        // style: 'style.css'
     },
     output: {
-        filename: 'encrypted.js',
-        // output files folder
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '/public'),
+        filename: 'bundle.js',
+        assetModuleFilename: 'assets/images/[name]-[hash][ext]'
     },
-    // set which loaders must be used for which file types
+    // For convenient assembly of sources, that will be uploaded to the server
+    devServer: {
+        static: './public',
+        port: 9000,
+        hot: true
+    },
     module: {
         rules: [
             // no rule required for js
             {
                 test: /\.css$/i, // Правило для CSS файлов
-                use: ['style-loader', 'css-loader'],
+                use: [miniCss.loader, 'style-loader', 'css-loader'],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i, // Правило для изображений
                 type: 'asset/resource',
             },
-            {
-                test: /\.html$/i, // Правило для HTML файлов
-                loader: 'html-loader',
-            },
+            //// Правило для HTML файлов
+            // {
+            //     test: /\.html$/i, 
+            //     loader: 'html-loader',
+            // },
         ],
     },
-    // TO analyze weight of dependencies
     plugins: [
         new BundleAnalyzerPlugin(),
-        new HtmlWebpackPlugin({
-            template: './public/index.html', // Исходный HTML файл
-            filename: 'encrypted.html', // Выходной HTML файл
-            template: './public/index_eng.html',
-            filename: 'encrypted_eng.html',
-          }),
+        new miniCss({
+            filename: 'styles.css',
+        }),
     ],
-    // For convenient assembly of sources, that will be uploaded to the server
-    devServer: {
-        static: path.join(__dirname, 'public'),
-        port: 9000
-    }
+
 };
 
